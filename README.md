e359fd955c6a53ff0d1abeb7641200b52d24e80c
sks72589 <sandeep2k2656@gmail.com>
2021-01-09 20:03:24 +0530

    v 0.0.4

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Angular Tuturial
 New app to Create Login Page Using angular and MySql
 v 0.0.2 : Basic App created
 v 0.0.3 : Login App created
+v 0.0.4 : Implemented Add and Substract(unstable:chances of error)
+v 0.0.5 : Implemented Add and Substract(stable)
+
+
+
+
---
+++ basics/basicapp/src/app/arithmatic/arithmatic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arithmatic',
+  templateUrl: './arithmatic.component.html',
+  styleUrls: ['./arithmatic.component.css']
+})
+export class ArithmaticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
